a50017c2b3387a788698b12b9637cb5db0853dbf
nguyenthanhtam96310 <nguyenthanhtam96310@gmail.com>
2025-08-28 17:09:07 +0700

    api_gateway

--- profile_service/src/main/java/com/book_micro/profile_service/controller/InternalUserProfileController.java
+++ profile_service/src/main/java/com/book_micro/profile_service/controller/InternalUserProfileController.java
@@ -1,9 +1,10 @@
 package com.book_micro.profile_service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.book_micro.profile_service.dto.request.UserProfileCreationRequest;
 import com.book_micro.profile_service.dto.response.UserProfileResponse;
 import com.book_micro.profile_service.service.UserProfileService;
 
@@ -14,11 +15,11 @@ import lombok.experimental.FieldDefaults;
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class UserProfileController {
+public class InternalUserProfileController {
     UserProfileService userProfileService;
 
-    @GetMapping("/users/{profileId}")
-    UserProfileResponse getProfile(@PathVariable String profileId) {
-        return userProfileService.getProfile(profileId);
+    @PostMapping("/internal/users")
+    UserProfileResponse createProfile(@RequestBody UserProfileCreationRequest request) {
+        return userProfileService.createProfile(request);
     }
 }
